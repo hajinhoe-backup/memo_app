fa4e6b6b02aadba3a96723ac804f41e3696fa0a1
jinho <jinhot@gmail.com>
2020-02-10 20:44:06 +0900

    Initial Commit

---
+++ memo_appTests/memo_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  memo_appTests.swift
+//  memo_appTests
+//
+//  Created by jinho on 2020/02/10.
+//  Copyright © 2020 jinho. All rights reserved.
+//
+
+import XCTest
+@testable import memo_app
+
+class memo_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
